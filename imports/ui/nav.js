191bcc1b726f722e522b034e553970edcbd4462b
JaylanT <Jaylan.Tse@gmail.com>
2016-11-30 22:12:36 -0800

    Styling and mobile navbar

--- imports/ui/nav.js
+++ imports/ui/nav.js
@@ -3,5 +3,9 @@ import { Template } from 'meteor/templating';
 import './nav.html';
 
 Template.nav.onRendered(() => {
+    $(".button-collapse").sideNav({
+        closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        draggable: true // Choose whether you can drag to open on touch screens
+    });
     $(".dropdown-button").dropdown();
 });
