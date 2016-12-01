3b3ceded0a898f80aaef346ae0888c1735af8ada
JaylanT <Jaylan.Tse@gmail.com>
2016-11-30 17:46:15 -0800

    Sort listings by newest

--- imports/ui/browse.js
+++ imports/ui/browse.js
@@ -11,7 +11,7 @@ Template.listings.onCreated(function () {
 
 Template.listings.helpers({
     listings() {
-        return Listings.find();
+        return Listings.find({}, { sort: { createdAt: -1 } });
     },
 });
 
